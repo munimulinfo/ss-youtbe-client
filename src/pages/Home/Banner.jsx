b1f23892e3121e8ef2ad287a2da07acdf50ba993
munimulinfo <munimul.info@gmail.com>
2024-06-03 00:15:09 +0600

    new code added

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,8 +1,43 @@
 import { useTranslation } from "react-i18next";
 import { FaArrowRight, FaDownload, FaYoutube } from "react-icons/fa6";
-
+import { useGetVedioInfoMutation } from "../../redux/baseApi";
+import { useState } from "react";
+const getYouTubeVideoID = (url) => {
+  const regex =
+    /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  const matches = url.match(regex);
+  return matches ? matches[1] : null;
+};
 const Banner = () => {
   const { t } = useTranslation();
+  const [videoInfo, setVideoInfo] = useState(null);
+  const [videoLink, setVideoLink] = useState("");
+  // const [downloadVedio, { isLoading, isError }] = useDownloadVedioMutation();
+  const [getVedioInfo] = useGetVedioInfoMutation();
+  const getYoutubeVideoInfo = async () => {
+    try {
+      const res = await getVedioInfo({ link: videoLink });
+      if (res) {
+        setVideoInfo(res?.data?.videoInfo);
+        console.log(res?.data?.videoInfo);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const video_download = (resu) => {
+    const videoId = getYouTubeVideoID(videoLink);
+    if (videoId) {
+      const url = `http://localhost:5000/video-download?id=${videoId}&resu=${String(
+        resu
+      )}`;
+      window.location.href = url;
+    } else {
+      alert("Invalid YouTube URL");
+    }
+  };
+
   return (
     <div className="max-w-6xl mx-auto py-24">
       <div className="lg:w-4/5 mx-auto">
@@ -10,11 +45,15 @@ const Banner = () => {
       </div>
       <div className="flex gap-2 lg:w-1/2 mx-auto md:flex-row bg-white p-2 rounded-md">
         <input
-          type="text"
+          type="url"
           className="grow focus:outline-none text-black"
           placeholder={t("banner-input-placeholder")}
+          onChange={(e) => setVideoLink(e.target.value)}
         />
-        <button className="btn bg-[#DC3545] text-white w-40px ">
+        <button
+          onClick={getYoutubeVideoInfo}
+          className="btn bg-[#DC3545] text-white w-40px "
+        >
           <span className="hidden md:inline-flex">{t("download-btn")} </span>{" "}
           <FaArrowRight className="md:text-lg" />
         </button>
@@ -24,48 +63,40 @@ const Banner = () => {
         <h2 className="text-xl font-bold my-4">Download video as:</h2>
         <div className="flex justify-center items-center gap-10 md:flex-row flex-col">
           <div>
-            <img className="w-64 mb-3" src="https://www.shutterstock.com/shutterstock/photos/761313844/display_1500/stock-photo--d-illustration-of-musical-notes-and-musical-signs-of-abstract-music-sheet-songs-and-melody-concept-761313844.jpg" alt="video img" />
-            <h3 className="text-left mb-3">পৃথিবীতে কেউ স্থায়ী ...</h3>
-            <p className="text-xl">Durations:</p>
+            <img
+              className="w-64 mb-3"
+              src={videoInfo?.thumbnailUrl}
+              alt="video img"
+            />
+            <h3 className="text-left mb-3">{videoInfo?.title}</h3>
+            <p className="text-xl">19:00</p>
           </div>
           <div className="md:w-[500px]">
             <h2 className="text-[#DC3545] flex justify-center items-center text-2xl gap-2 font-bold border border-[#1600A9] p-2 border-b-0">
               <FaYoutube />
               Video
             </h2>
-            <div className="flex justify-between items-center border border-[#1600A9]  p-3 border-t-2 border-b-[#393361]">
-              <p className="font-bold">720.mp4</p>
-              <button className="flex items-center gap-1 btn text-[#DC3545] p-2 w-36 btn-ghost bg-white text-center">
-                <FaDownload />
-                Download
-              </button>
-            </div>
-            <div className="flex justify-between items-center border border-[#1600A9]  p-3 border-b-[#393361] border-t-[#393361]">
-              <p className="font-bold">360.mp4</p>
-              <button className="flex items-center gap-1 btn text-[#DC3545] p-2 w-36 btn-ghost bg-white text-center">
-                <FaDownload />
-                Download
-              </button>
-            </div>
-            <div className="flex justify-between items-center border border-[#1600A9]  p-3 border-b-[#393361] border-t-[#393361]">
-              <p className="font-bold">1080.mp4</p>
-              <button className="flex items-center gap-1 btn text-[#DC3545] p-2 w-36 btn-ghost bg-white text-center">
-                <FaDownload />
-                Download
-              </button>
-            </div>
-            <div className="flex justify-between items-center border border-[#1600A9]  p-3 border-t-[#393361] border-b-[#393361]">
-              <p className="font-bold">720.mp4</p>
-              <button className="flex items-center gap-1 btn text-[#DC3545] p-2 w-36 btn-ghost bg-white text-center">
-                <FaDownload />
-                Download
-              </button>
-            </div>
-            <div className="border border-[#1600A9] p-3 border-t-[#393361] ">
-              <button className="text-[#fff] p-2 w-36 text-center">
-                Show More
-              </button>
-            </div>
+            {videoInfo?.videoResu?.length > 0 && (
+              <div>
+                {videoInfo?.videoResu?.map((resu, index) => (
+                  <div
+                    key={index}
+                    className="flex justify-between items-center border border-[#1600A9]  p-3 border-t-2 border-b-[#393361]"
+                  >
+                    <p className="font-bold">{resu}.mp4</p>
+                    <button
+                      onClick={() => video_download(resu)}
+                      className="flex items-center gap-1 btn text-[#DC3545] p-2 w-36 btn-ghost bg-white text-center"
+                    >
+                      <p>
+                        <FaDownload />
+                        Download
+                      </p>
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/redux/baseApi.js
+++ src/redux/baseApi.js
@@ -1,12 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
   endpoints: (builder) => ({
     getVedioInfo: builder.mutation({
-      query: (name) => ({}),
+      query: ({ link }) => {
+        return {
+          url: "/get-video-info",
+          method: "POST",
+          body: { url: link },
+        };
+      },
+    }),
+    downloadVedio: builder.mutation({
+      query: ({ id, resu }) => {
+        return {
+          url: "/download",
+          method: "POST",
+          body: { url: id, quality: "" },
+        };
+      },
     }),
   }),
 });
 
-export const { useGetPokemonByNameQuery } = baseApi;
+export const { useGetVedioInfoMutation, useDownloadVedioMutation } = baseApi;
